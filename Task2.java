38db3b8bc104ac0bb8cda5d30cb868185db2160b
James Gambino <66328776+JGambino00@users.noreply.github.com>
2023-12-23 13:13:19 -0500

    Fixed typo in Task2.java

--- Task2.java
+++ Task2.java
@@ -8,7 +8,7 @@ public class Task2
 
     /* Approach: Initialize the sum to 0 and then iterate through each element of the array. If the element of the array is positive then add
     it to the sum. Finally, the sum will be returned. 
-    An optimization appr0ach can be to attempt using parallel processing. An alternate way of implementing this solution would be to have
+    An optimization approach can be to attempt using parallel processing. An alternate way of implementing this solution would be to have
     if(number < 0) continue; However, the time complexity for this would still be O(n) because each element of the array would still need
     to be visited in order to check if it is positive or not. */ 
     public static int sumOfPositiveIntegers(int[] arr){
